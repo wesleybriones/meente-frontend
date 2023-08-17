6919ddcc6fec38b97295fbc3bf65d69d629a0c43
Wesley Briones <wesgbrio@espol.edu.ec>
2023-08-17 10:24:44 -0500

    Update login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 
 export const LoginPage = () => {
 
   const navigate = useNavigate();
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
 
   const onLogin = () => {
     navigate('/', {
@@ -12,17 +16,35 @@ export const LoginPage = () => {
   }
 
   return (
-    <div className="container mt-5">
-      <h1>Login</h1>
-      <hr />
+    <div className="container w-25 d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <h1 className="text-center">Inicio de Sesión</h1>
+        <hr className="w-100 text-primary"/>
 
-      <button 
-        className="btn btn-primary"
-        onClick={ onLogin }
-      >
-        Ingresar
-      </button>
+        <form>
+          <div>
+            <label className="col-4">Usuario:</label>
+            <input 
+              className="col-8 border-primary" 
+              type="text"
+            />
+          </div>
 
+          <div className="mt-3 mb-3">
+            <label className="col-4">Contraseña:</label>
+            <input 
+              className="col-8 border-primary" 
+              type="text"
+              autoComplete="off"
+            />
+          </div>
+
+        </form>
+        <button 
+          className="btn btn-primary w-100"
+          onClick={ onLogin }
+        >
+          Ingresar
+        </button>
     </div>
   )
 }
